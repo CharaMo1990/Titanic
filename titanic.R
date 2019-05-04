d66adcf7f28de1aa0635f2f359598d26ab3eda49
CharaMo1990 <charamou@126.com>
2019-05-04 19:19:06 +0800

    just4test

---
+++ titanic.R
@@ -0,0 +1,6 @@
+> library(ggplot2)
+Error in library(ggplot2) : 不存在叫‘ggplot2’这个名字的程辑包
+> library(dply)
+Error in library(dply) : 不存在叫‘dply’这个名字的程辑包
+> library(mice)
+Error in library(mice) : 不存在叫‘mice’这个名字的程辑
